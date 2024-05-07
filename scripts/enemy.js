2be683138e78d706c05adf98b6d71659b0235db4
Herbert Nordson <herbert_nordson@hotmail.com>
2024-05-07 08:49:56 -0300

    testt

--- scripts/enemy.js
+++ scripts/enemy.js
@@ -22,7 +22,7 @@ const Enemy = ({ obstacleElement, flyElement, level, newLevel }) => {
       setTimeout(() => {
         flyElement.classList.remove("invisible");
         randomiseEnemyPosition();
-      }, 3000);
+      }, 1000);
       return;
     }
   }
